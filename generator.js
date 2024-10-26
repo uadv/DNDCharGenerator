01d56814f53f0a1a32a23cead35ed63c5af4358d
Adzy(uadv) <imadzgg@gmail.com>
2024-10-26 12:49:45 +0100

    Add the data to the Script

---
+++ generator.js
@@ -0,0 +1,8 @@
+// This is the data it can randomize through
+
+const skinCol = ['Red', 'Green', 'Blue', 'Yellow', 'Purple', 'Orange', 'Pink', 'Cyan', 'Magenta', 'Gray']
+const race = ['Human', 'Elf', 'Dwarf', 'Halfling', 'Dragonborn', 'Gnome', 'Half-Elf', 'Half-Orc', 'Tiefling']
+const weapon = ['Longsword', 'Shortbow', 'Warhammer', 'Dagger', 'Greatsword', 'Mace', 'Crossbow', 'Battleaxe', 'Spear']
+const eyeCol = ['Red', 'Green', 'Blue', 'Yellow', 'Purple', 'Orange', 'Pink', 'Cyan', 'Magenta', 'Gray']
+const ability = ['Healing', 'Attacking', 'Defending']
+let height;
